0f050c75d84cae9d2f3279ae8d7dcf4cccaa0306
Ura Takato <e245751@ie.u-ryukyu.ac.jp>
2026-04-28 09:20:47 +0900

    add regression.fit

--- src/main.py
+++ src/main.py
@@ -1,5 +1,6 @@
 import datasets
 import regression
+import importlib
 
 X,Y=datasets.load_linear_example1()
 print(X)
@@ -7,3 +8,6 @@ print(Y)
 
 model=regression.LinearRegression()
 print(model.x)
+
+model.fit(X,Y)
+print(model.theta)
