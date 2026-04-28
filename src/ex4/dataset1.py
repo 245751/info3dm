f23300da334ec311625a60828d903ba8fc2559fd
Ura Takato <e245751@ie.u-ryukyu.ac.jp>
2026-04-28 11:36:54 +0900

    compleate ex1.2

--- src/ex4/dataset1.py
+++ src/ex4/dataset1.py
@@ -1,5 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import pandas as pd
+np.random.seed(0)
 
 def true_function(x):
     '''
@@ -11,10 +13,17 @@ def true_function(x):
 
 X=np.arange(-1,2,0.01)
 Y=true_function(X)
-plt.plot(X,Y)
-plt.xlabel("X")
-plt.ylabel("Y")
-plt.title("Exercise1.1 graph")
+
+X_Coordinates=np.random.uniform(-1,1,20)
+Y_Coordinates=true_function(X_Coordinates)
+data=np.array([[x,y] for x,y in zip(X_Coordinates,Y_Coordinates)])
+df=pd.DataFrame(data,columns=["観測点","真値"])
+fig,ax=plt.subplots()
+ax.set_xlabel("X")
+ax.set_ylabel("Y")
+ax.set_title("Exercise1.2 graph")
+ax.plot(X,Y)
+ax.plot(X_Coordinates,Y_Coordinates,".")
 plt.show()
 if __name__=="__main__":
     import doctest
