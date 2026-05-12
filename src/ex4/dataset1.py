0f63c5aa5c14e2949de9b9472b2684440213f301
Ura Takato <e245751@ie.u-ryukyu.ac.jp>
2026-05-12 11:19:01 +0900

    compleate ex1.6

--- src/ex4/dataset1.py
+++ src/ex4/dataset1.py
@@ -12,9 +12,31 @@ def true_function(x):
     '''
     return np.sin(np.pi*x*0.8)*10
 
+def load_dataset():
+    X=np.arange(-1,np.sqrt(2),0.01)
+    Y=true_function(X)
+    return X,Y
+
+def load_noisy_dataset():
+    X,Y=load_dataset()
+    X_Coordinates=np.random.uniform(-1,1,20)
+    Y_Coordinates=true_function(X_Coordinates)
+    noise=np.random.normal(0,1,20)/2
+    Y_Coordinates_Noisy=Y_Coordinates+noise
+    data=np.array([[x,y] for x,y in zip(X_Coordinates,Y_Coordinates)])
+    df=pd.DataFrame(data,columns=["観測点","真値"])
+    df["観測値"]=Y_Coordinates_Noisy
+    return X_Coordinates, Y_Coordinates_Noisy,df
+
+def save_dataset(df):
+    df.to_csv("src/ex4/dataset1.tsv",sep="\t",index=False)
+
+def output_dataset():
+    df=pd.read_csv("src/ex4/dataset1.tsv",sep="\t")
+    return df
+
 X=np.arange(-1,np.sqrt(2),0.01)
 Y=true_function(X)
-
 X_Coordinates=np.random.uniform(-1,1,20)
 noise=np.random.normal(0,1,20)/2
 Y_Coordinates=true_function(X_Coordinates)
