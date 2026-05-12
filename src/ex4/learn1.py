28a5bc9a14268c2fe052da34dffe726e9ec60574
Ura Takato <e245751@ie.u-ryukyu.ac.jp>
2026-05-12 11:27:53 +0900

    compleate ex1.9

--- src/ex4/learn1.py
+++ src/ex4/learn1.py
@@ -4,6 +4,8 @@ import matplotlib.pyplot as plt
 import japanize_matplotlib
 import pandas as pd
 from sklearn.linear_model import LinearRegression
+from sklearn.model_selection import train_test_split
 
 X,Y=dataset1.load_dataset()
+X_train, X_test, y_train, y_test = train_test_split(X, Y, test_size=0.2, random_state=0)
 model=LinearRegression()
