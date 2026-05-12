87ca2b75cac7d5d6c61c7d5083edd1bc8d89793f
Ura Takato <e245751@ie.u-ryukyu.ac.jp>
2026-05-12 11:25:22 +0900

    compleate ex1.8

--- src/ex4/learn1.py
+++ src/ex4/learn1.py
@@ -3,5 +3,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import japanize_matplotlib
 import pandas as pd
+from sklearn.linear_model import LinearRegression
 
 X,Y=dataset1.load_dataset()
+model=LinearRegression()
