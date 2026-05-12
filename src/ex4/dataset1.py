b12a4d35d2252f777d3e413fa3fb2c5414f03b05
Ura Takato <e245751@ie.u-ryukyu.ac.jp>
2026-05-12 11:39:51 +0900

    compleate ex1.10

--- src/ex4/dataset1.py
+++ src/ex4/dataset1.py
@@ -35,27 +35,27 @@ def output_dataset():
     df=pd.read_csv("src/ex4/dataset1.tsv",sep="\t")
     return df
 
-X=np.arange(-1,np.sqrt(2),0.01)
-Y=true_function(X)
-X_Coordinates=np.random.uniform(-1,1,20)
-noise=np.random.normal(0,1,20)/2
-Y_Coordinates=true_function(X_Coordinates)
-Y_Coordinates_Noisy=Y_Coordinates+noise
-data=np.array([[x,y] for x,y in zip(X_Coordinates,Y_Coordinates)])
-df=pd.DataFrame(data,columns=["観測点","真値"])
-df["観測値"]=Y_Coordinates_Noisy
-load_df=pd.read_csv("src/ex4/dataset1.tsv",sep="\t")
-print(load_df)
-exit()
-fig,ax=plt.subplots()
-ax.set_xlabel("X")
-ax.set_ylabel("Y")
-ax.set_title("Exercise1.3 graph")
-ax.plot(X,Y,color="blue",label="sin(pi*0.8*x)*10")
-ax.plot(X_Coordinates,Y_Coordinates,".",color="black",label="真値")
-ax.scatter(X_Coordinates,Y_Coordinates_Noisy,color="red",label="観測値")
-ax.legend()
-plt.show()
 if __name__=="__main__":
+    X=np.arange(-1,np.sqrt(2),0.01)
+    Y=true_function(X)
+    X_Coordinates=np.random.uniform(-1,1,20)
+    noise=np.random.normal(0,1,20)/2
+    Y_Coordinates=true_function(X_Coordinates)
+    Y_Coordinates_Noisy=Y_Coordinates+noise
+    data=np.array([[x,y] for x,y in zip(X_Coordinates,Y_Coordinates)])
+    df=pd.DataFrame(data,columns=["観測点","真値"])
+    df["観測値"]=Y_Coordinates_Noisy
+    load_df=pd.read_csv("src/ex4/dataset1.tsv",sep="\t")
+    print(load_df)
+    exit()
+    fig,ax=plt.subplots()
+    ax.set_xlabel("X")
+    ax.set_ylabel("Y")
+    ax.set_title("Exercise1.3 graph")
+    ax.plot(X,Y,color="blue",label="sin(pi*0.8*x)*10")
+    ax.plot(X_Coordinates,Y_Coordinates,".",color="black",label="真値")
+    ax.scatter(X_Coordinates,Y_Coordinates_Noisy,color="red",label="観測値")
+    ax.legend()
+    plt.show()
     import doctest
     doctest.testmod()
--- src/ex4/learn1.py
+++ src/ex4/learn1.py
@@ -8,4 +8,11 @@ from sklearn.model_selection import train_test_split
 
 X,Y=dataset1.load_dataset()
 X_train, X_test, y_train, y_test = train_test_split(X, Y, test_size=0.2, random_state=0)
+X_train=X_train.reshape(-1,1)
+X_test=X_test.reshape(-1,1)
 model=LinearRegression()
+model.fit(X_train,y_train)
+y_pred=model.predict(X_test)
+print("予測値:",y_pred)
+print("真値:",y_test)
+print("スコア",model.score(X_test,y_test))
