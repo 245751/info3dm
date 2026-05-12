c397a5135f12dcf9266f41ff04ab45e3a1d4432d
Ura Takato <e245751@ie.u-ryukyu.ac.jp>
2026-05-12 10:48:33 +0900

    compleate ex1.3

--- src/ex4/dataset1.py
+++ src/ex4/dataset1.py
@@ -1,5 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import japanize_matplotlib
 import pandas as pd
 np.random.seed(0)
 
@@ -11,19 +12,25 @@ def true_function(x):
     '''
     return np.sin(np.pi*x*0.8)*10
 
-X=np.arange(-1,2,0.01)
+X=np.arange(-1,np.sqrt(2),0.01)
 Y=true_function(X)
 
 X_Coordinates=np.random.uniform(-1,1,20)
+noise=np.random.normal(0,1,20)/2
 Y_Coordinates=true_function(X_Coordinates)
+Y_Coordinates_Noisy=Y_Coordinates+noise
 data=np.array([[x,y] for x,y in zip(X_Coordinates,Y_Coordinates)])
 df=pd.DataFrame(data,columns=["観測点","真値"])
+df["観測値"]=Y_Coordinates_Noisy
+print(df)
 fig,ax=plt.subplots()
 ax.set_xlabel("X")
 ax.set_ylabel("Y")
-ax.set_title("Exercise1.2 graph")
-ax.plot(X,Y)
-ax.plot(X_Coordinates,Y_Coordinates,".")
+ax.set_title("Exercise1.3 graph")
+ax.plot(X,Y,color="blue",label="sin(pi*0.8*x)*10")
+ax.plot(X_Coordinates,Y_Coordinates,".",color="black",label="真値")
+ax.scatter(X_Coordinates,Y_Coordinates_Noisy,color="red",label="観測値")
+ax.legend()
 plt.show()
 if __name__=="__main__":
     import doctest
